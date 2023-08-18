631e802a516581f4a76a91de85dc66909bae5259
isowoogi <bkg21@naver.com>
2023-08-18 22:31:10 +0900

    updated

--- video_maker/usecases/create_thumbnail.py
+++ video_maker/usecases/create_thumbnail.py
@@ -10,6 +10,7 @@ from entities.match_data import MatchData
 from entities.progress import print_progress
 from bs4 import BeautifulSoup
 
+
 class CreateThumbnail:
     def __init__(self, data_scrapper: DataScrapper, data: MatchData) -> None:
         self.scrapper = data_scrapper
@@ -17,61 +18,76 @@ class CreateThumbnail:
         self.__thumb_path = os.path.abspath(r'.\media\thumb\thumb.png')
         self.__replay_file_dir = os.path.abspath(r'.\media\replays')
         file = os.listdir(self.__replay_file_dir)[0].split(".")[0]
-        self.__static_thumb_path = os.path.join(os.path.abspath(r'.\media\AllThumbs'),f'{file}.png')
-        self.total=100
+        self.__static_thumb_path = os.path.join(
+            os.path.abspath(r'.\media\AllThumbs'), f'{file}.png')
+        self.total = 100
         print_progress(1, self.total, prefix='Creating Thumbnail:')
-        self.skins ={
-            "Yuumi":[0,1,11,19,28,37,39],
+        self.skins = {
+            "Yuumi": [0, 1, 11, 19, 28, 37, 39],
         }
-    def exceptionHandle(self,name):
-        # print(name)
-        # if(name == "Wukong"):
-        #     return "MonkeyKing"
-        if(name == "KaiSa"):
-            return "Kai-sa"
-        elif(name == "KhaZix"):
-            return "kha-zix"
-        elif(name == "NunuWillump"):
-            return "Nunu"
-        elif(name == "BelVeth"):
-            return "Bel-veth"
-        elif(name == "RenataGlasc"):
-            return "Renata"
-        elif(name == "TwistedFate"):
-            return "Twisted-fate"
-        elif(name == "LeeSin"):
-            return "lee-sin"
-        elif(name == "RekSai"):
-            return "rek-sai"
-        elif(name == "KSante"):
-            return "k-sante"
-        elif(name == "KogMaw"):
-            return "Kog-maw"
-        elif(name == "JarvanIV"):
-            return "jarvan-iv"
-        else: return name
-        
-    def iconReplace(self,name):
 
-        if(name == "KaiSa"):
-            return "Kaisa"
-        elif(name == "KhaZix"):
-            return "Khazix"
-        elif(name == "NunuWillump"):
+    def exceptionHandle(self, name):
+        # print(name)
+        if(name == "AurelionSol"):
+            return "aurelion-sol"
+        elif (name == "KaiSa"):
+            return "Kai-sa"
+        elif (name == "VelKoz"):
+            return "vel-koz"
+        elif (name == "KhaZix"):
+            return "kha-zix"
+        elif (name == "NunuWillump"):
             return "Nunu"
-        elif(name == "BelVeth"):
+        elif (name == "BelVeth"):
             return "Belveth"
-        elif(name == "RenataGlasc"):
+        elif (name == "RenataGlasc"):
             return "Renata"
-        elif(name == "RekSai"):
+        elif (name == "TwistedFate"):
+            return "Twisted-fate"
+        elif (name == "LeeSin"):
+            return "lee-sin"
+        elif (name == "RekSai"):
+            return "rek-sai"
+        elif (name == "KSante"):
+            return "k-sante"
+        elif (name == "KogMaw"):
+            return "Kog-maw"
+        elif (name == "JarvanIV"):
+            return "jarvan-iv"
+        elif (name == "MasterYi"):
+            return "Master-yi"
+        else:
+            return name
+
+    def iconReplace(self, name):
+        if (name == "KaiSa"):
+            return "Kaisa"
+        elif (name == "VelKoz"):
+            return "Velkoz"
+        elif (name == "KhaZix"):
+            return "Khazix"
+        elif (name == "NunuWillump"):
+            return "Nunu"
+        elif (name == "BelVeth"):
+            return "Belveth"
+        elif (name == "RenataGlasc"):
+            return "Renata"
+        elif (name == "TwistedFate"):
+            return "Twisted-fate"
+        elif (name == "LeeSin"):
+            return "lee-sin"
+        elif (name == "RekSai"):
             return "RekSai"
-        elif(name == "Wukong"):
+        elif (name == "Wukong"):
             return "MonkeyKing"
-        else: return name
+        else:
+            return name
+
     def getSkin(self, name):
         # url = "https://www.leagueoflegends.com/en-gb/champions/{name}/"
         # make get request and use beautifulsoup and find the skin img urls
-        url = "https://www.leagueoflegends.com/en-gb/champions/{}/".format(name)
+        url = "https://www.leagueoflegends.com/en-gb/champions/{}/".format(
+            name)
         # print(url)
         r = requests.get(url)
         # if r.status_code is '404':
@@ -80,16 +96,18 @@ class CreateThumbnail:
         soup = BeautifulSoup(r.text, 'html.parser')
         skinsImgTag = soup.find_all('img')
         skinsUrls = list(set([skin.get('src') for skin in skinsImgTag]))
-        filtered_urls = [skinUrl for skinUrl in skinsUrls if skinUrl is not None and "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/" in skinUrl]
-
+        filtered_urls = [
+            skinUrl for skinUrl in skinsUrls if skinUrl is not None and "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/" in skinUrl]
+        
         return filtered_urls
-    
+
     def create_thumbnail(self):
         print_progress(5, self.total, prefix='Creating Thumbnail:')
         champion = self.lol_data['mvp']['champion']
+        championRaw = champion
         champion = champion.replace("'", "& ")
-        champion = champion.replace("&","")
-        if(len(champion.split())==1):
+        champion = champion.replace("&", "")
+        if (len(champion.split()) == 1):
             champion = champion.capitalize()
         champion = champion.replace(" ", "")
         # champion = self.lol_data['mvp']['champion']
@@ -99,53 +117,54 @@ class CreateThumbnail:
         print_progress(8, self.total, prefix='Creating Thumbnail:')
         # if champion=="KaiSa":
         #     champion=="Kaisa"
-        rank=self.lol_data['mvp']['rank']
-        ranks= {
+        rank = self.lol_data['mvp']['rank']
+        ranks = {
             "Iron": "https://lolg-cdn.porofessor.gg/img/s/league-icons-v3/160/1.png",
             "Bronze": "https://lolg-cdn.porofessor.gg/img/s/league-icons-v3/160/2.png",
             "Silver": "https://lolg-cdn.porofessor.gg/img/s/league-icons-v3/160/3.png",
             "Gold": "https://lolg-cdn.porofessor.gg/img/s/league-icons-v3/160/4.png",
             "Platinum": "https://lolg-cdn.porofessor.gg/img/s/league-icons-v3/160/5.png",
-            "Emerald": "https://lolg-cdn.porofessor.gg/img/s/league-icons-v3/160/6.png",
-            "Diamond": "https://lolg-cdn.porofessor.gg/img/s/league-icons-v3/160/7.png",
-            "Master": "https://lolg-cdn.porofessor.gg/img/s/league-icons-v3/160/8.png",
-            "GrandMaster": "https://lolg-cdn.porofessor.gg/img/s/league-icons-v3/160/9.png",
-            "Challenger": "https://lolg-cdn.porofessor.gg/img/s/league-icons-v3/160/10.png"
+            "Diamond": "https://lolg-cdn.porofessor.gg/img/s/league-icons-v3/160/6.png",
+            "Master": "https://lolg-cdn.porofessor.gg/img/s/league-icons-v3/160/7.png",
+            "GrandMaster": "https://lolg-cdn.porofessor.gg/img/s/league-icons-v3/160/8.png",
+            "Challenger": "https://lolg-cdn.porofessor.gg/img/s/league-icons-v3/160/9.png"
         }
         print_progress(12, self.total, prefix='Creating Thumbnail:')
-        rankIcon=ranks.get(rank)
-        if(rankIcon is None):
-            rankIcon="https://lolg-cdn.porofessor.gg/img/s/league-icons-v3/160/9.png"
-        spellImgs=os.listdir("assets/img/spell")
+        rankIcon = ranks.get(rank)
+        if (rankIcon is None):
+            rankIcon = "https://lolg-cdn.porofessor.gg/img/s/league-icons-v3/160/9.png"
+        spellImgs = os.listdir("assets/img/spell")
         print_progress(19, self.total, prefix='Creating Thumbnail:')
-        spellImg=random.sample(spellImgs, 3)
-        spellImgNew=self.lol_data['mvp']['spell']
-        spellImgNew=[s+'.png' for s in spellImgNew]
-        spellImg=spellImgNew
+        spellImg = random.sample(spellImgs, 3)
+        spellImgNew = self.lol_data['mvp']['spell']
+        spellImgNew = [s+'.png' for s in spellImgNew]
+        spellImg = spellImgNew
         print_progress(25, self.total, prefix='Creating Thumbnail:')
-        
-        loser=self.lol_data['loser']
-        imgUrl=""
-        count=0
+
+        loser = self.lol_data['loser']
+        imgUrl = ""
+        count = 0
         # print(champion)
         # print("match region:",self.lol_data['region'],os.path.exists(f"assets/img/{self.lol_data['region']}.png"))
-        if(os.path.exists(f"assets/img/{self.lol_data['region']}.png")):
-            region=self.lol_data['region']
+        if (os.path.exists(f"assets/img/{self.lol_data['region']}.png")):
+            region = self.lol_data['region']
         else:
-            region="EUW"
+            region = "EUW"
         # print(champion)
         skins = self.getSkin(champion)
         # print(skins)
-        if(len(skins)==0):
+        if (len(skins) == 0):
             print("\n==> Thumbnail creation failed \n==> Sent message to developer!\n==> Take a screenshot and sent mail to : tamilcomway@gmail.com")
             # print("==> Champion Temp: " + champonTemp)
             print("==> Champion name: " + str(champion))
-            print("==> URL:",imgUrl)
+            print("==> Raw Champion name: " + str(championRaw))
+            print("==> URL:", imgUrl)
             return False
-        imgUrl=random.choice(skins)
+        imgUrl = random.choice(skins)
 
         print_progress(40, self.total, prefix='Creating Thumbnail:')
-        oppIconImg = self.iconReplace(championTemp.replace(" ",""))
+        oppIconImg = self.iconReplace(championTemp.replace(" ", ""))
+        loserIcon = self.iconReplace(loser.replace(" ", ""))
         self.__create_html(
             kda=self.lol_data['mvp']['kda'].split("/"),
             imgUrl=imgUrl,
@@ -156,12 +175,13 @@ class CreateThumbnail:
             rankIcon=rankIcon,
             spellImg=spellImg,
             opponentIcon=f'https://opgg-static.akamaized.net/meta/images/lol/champion/{oppIconImg}.png',
-            region=region
+            region=region,
+            loserIcon=f'https://opgg-static.akamaized.net/meta/images/lol/champion/{loserIcon}.png',
         )
         print_progress(50, self.total, prefix='Creating Thumbnail:')
         html_path = os.path.abspath('assets/thumbnail.html')
         self.scrapper.driver.get('file://' + html_path)
-        timer=51
+        timer = 51
         for i in range(10):
             sleep(0.5)
             print_progress(timer+i*2, self.total, prefix='Creating Thumbnail:')
@@ -175,11 +195,11 @@ class CreateThumbnail:
             img.save(self.__thumb_path, quality=70)
             img.save(self.__static_thumb_path, quality=70)
         print_progress(100, self.total, prefix='Creating Thumbnail:')
-        
+
         self.scrapper.driver.quit()
         return True
 
-    def __create_html(self, kda: str, mvp: str, vs: str, rank: str, patch: str, imgUrl: str,rankIcon:str,spellImg:list,opponentIcon:str,region):
+    def __create_html(self, kda: str, mvp: str, vs: str, rank: str, patch: str, imgUrl: str, rankIcon: str, spellImg: list, opponentIcon: str, region, loserIcon):
         none_vars = []
         if kda is None:
             none_vars.append('kda')
@@ -200,194 +220,31 @@ class CreateThumbnail:
         if none_vars:
             # print(f"One or more arguments are None: {', '.join(none_vars)}")
             return
-        HTML = """<!DOCTYPE html>
-<html>
-<head>
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Nanum+Gothic:wght@400;800&display=swap" rel="stylesheet">
-    <style>
-        * {
-            margin: 0;
-            padding: 0;
-            box-sizing: border-box;
-            font-family: 'Nanum Gothic', sans-serif;
-        }
-        .container {
-            background-image: url('"""+imgUrl.replace("'","")+"""');
-            background-size: cover;
-            width: 1280px;
-            height: 720px;
-            display: flex;
-            align-items: center;
-            position: relative; /* Add position relative to container */
-        }
-        .frame {
-            width: 500px;
-            height: 650px;
-            margin-left: 50px;
-            background-color: rgba(0, 0, 0, .6);
-            filter: drop-shadow(0px 20px 10px rgba(0, 0, 0, 0.3));
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-            justify-content: space-between;
-            font-weight: 800;
-        }
-        .kda {
-            margin-top: 2rem;
-            color: white;
-            font-size: 2.5rem;
-            display: flex;
-            justify-content: center;
-            background-color: rgba(0, 0, 0, .5);
-            filter: drop-shadow(0px 20px 10px rgba(0, 0, 0, 0.3));
-            border-radius:50%;
-            width:4rem;
-            height:4rem;
-            align-items: center;
-            border: 2px solid white;
-            margin: 2px -2px 5px 18px;
-        }
-        .match {
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-            color: white;
-        }
-        .mvp {
-            font-size: 3.5rem; /* set the font size to 10% of the viewport width */
-            position: absolute;
-            bottom: 158px;
-            white-space: nowrap;
-        }
-        .vs {
-            font-size: 2rem;
-        }
-        .region {
-            background-color: aquamarine;
-            color: #444;
-            padding: .5rem 1.5rem;
-            border-radius: 2rem;
-            font-size: 2rem;
-        }
-        .patch {
-            color: white;
-            font-size: 3rem;
-            position: absolute; /* Add position absolute to patch div */
-            top: 0; /* Position it at the top */
-            right: 0; /* Position it at the right */
-            margin: 3rem; /* Add margin to create some space */
-            background-color: rgba(0, 0, 0, .4);
-            filter: drop-shadow(0px 20px 10px rgba(0, 0, 0, 0.3));
-            font-weight: bold;
-        }
-        .kdatext{
-            font-size: 64px;
-            padding: 0px 2.5rem;
-            background-color: #c9c8c6;
-            color:black;
-            width:13rem;
-            font-weight: bold;
-            border-radius:2rem;
-            text-align-last: end;
-            align-self: self-end;
-            margin-bottom: 3px;
-        }
-        .line {
-            border: none;
-            border-top: 5px solid white;
-            margin: 1rem 2rem;
-            padding:2px;
-            width:450px;
-            color:white;
-            position: absolute;
-            bottom: 110px;
-          }
-          .players{
-            width:70px;
-            height:70px;
-            border-radius:50px;
-            background-color: rgba(255, 255, 255, .1);
-            filter: drop-shadow(0px 20px 10px rgba(0, 0, 0, 0.3));
-            border: 3px solid white;
-            margin: 0px 2px;
-          }
-          .players1{
-            width:167px;
-            height:167px;
-            border-radius:50%;
-            background-color: rgba(255, 255, 255, .1);
-            filter: drop-shadow(0px 20px 10px rgba(0, 0, 0, 0.3));
-            border: 5px solid white;
-            margin: 0px 14px;
-          }
-          .playerlist{
-            position: absolute;
-            bottom: 234px;
-          }
-          .mainplayers{
-            position: absolute;
-            bottom: 420px;
-          }
-          .challenger{
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-            color: white;
-            font-size: 3rem;
-            position: absolute;
-            bottom: 335px;
-            }
-            .bottom{
-                position: absolute;
-                bottom: 31px;
-            }
-    </style>
-</head>
-<body>
-    <div class="container">
-        <div class="frame">
-            <div class="mainplayers">
-                <img class="players1" src='"""+rankIcon+"""'>
-                <img class="players1" src='"""+opponentIcon+"""'/>
-            </div>
-            <div class="challenger">
-                <p>"""+rank+"""</p>
-            </div>
-            <div class="playerlist">
-                <img class="players" src='../assets/img/"""+region+""".png'/>
-                <img class="players" src='"""+spellImg[0]+"""'/>
-                <img class="players" src='../assets/img/spell/"""+spellImg[1]+"""'/>
-                <img class="players" src='../assets/img/spell/"""+spellImg[2]+"""'/>
-            </div>
-            <div class="match">
-                <p class="mvp">"""+mvp+"""</p>
-            </div>
-            <hr class="line"/>
-            <div class="bottom" style='display:flex'>
-                <p class="kdatext">KDA</p>
-                <p >
-                    <p class="kda">"""+kda[0]+"""</p>
-                    <p class="kda">"""+kda[1]+"""</p>
-                    <p class="kda">"""+kda[2]+"""</p>
-                </p>
-            </div>
-        </div>
-        <div class="patch">
-            <p>"""+patch+"""</p>
-        </div>
+        with open("./assets/template.html", "r", encoding='utf-8') as f:
+            HTML = f.read()
+            # print(HTML)
+        HTML = HTML.replace("backgroundImageLOL", imgUrl.replace("'", ""))
+        HTML = HTML.replace("rankIconLOL", rankIcon)
+        HTML = HTML.replace("loserIconLOL", loserIcon)
+        HTML = HTML.replace("opponentIconLOL", opponentIcon)
+        HTML = HTML.replace("patchLOL", patch)
+        HTML = HTML.replace("rankLOL", rank)
+        HTML = HTML.replace("regionLOL", region)
+        HTML = HTML.replace("spellImg0LOL", spellImg[0])
+        HTML = HTML.replace("spellImg1LOL", spellImg[1])
+        HTML = HTML.replace("spellImg2LOL", spellImg[2])
+        HTML = HTML.replace("mvpLOL", mvp)
+        HTML = HTML.replace("kda0LOL", kda[0])
+        HTML = HTML.replace("kda1LOL", kda[1])
+        HTML = HTML.replace("kda2LOL", kda[2])
 
-
-    </div>
-</body>
-</html>
-"""
-        with open("./assets/thumbnail.html", "w",encoding='utf-8') as f:
+        with open("./assets/thumbnail.html", "w", encoding='utf-8') as f:
             f.write(HTML)
 
-if __name__ ==  "__main__":
+
+if __name__ == "__main__":
     from usecases.data import load
     lol_data: MatchData = load()
-    thumb_creator = CreateThumbnail(data_scrapper=DataScrapper(), data=lol_data)
+    thumb_creator = CreateThumbnail(
+        data_scrapper=DataScrapper(), data=lol_data)
     thumb_creator.create_thumbnail()
